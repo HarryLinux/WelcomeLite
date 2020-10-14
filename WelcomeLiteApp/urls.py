7dab45efeb275a629d3a2cb10def7935147d47bf
Harold Fich <haroldfich@gmail.com>
2020-10-14 01:01:10 -0400

    Basic add&edit offer templates, link to views

--- WelcomeLiteApp/urls.py
+++ WelcomeLiteApp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'welcome'
 urlpatterns = [
     path('', views.index, name='index'),
     path('addOffer/', views.add_offer, name='add'),
--- WelcomeLiteApp/views.py
+++ WelcomeLiteApp/views.py
@@ -1,18 +1,20 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 
 from .models import JobOffer
 
 def index(request):
-    job_offer_list = JobOffer.objects.all()
+    job_offer_list = get_list_or_404(JobOffer)
     context = { 'job_offer_list': job_offer_list }
     return render(request, 'WelcomeLiteApp/index.html', context)
 
 def add_offer(request):
-    return HttpResponse("We made it to the add offers page!")
+    return render(request, 'WelcomeLiteApp/add_offer.html')
 
 def edit_offer(request, job_offer_id):
-    return HttpResponse("You're editing offer %s" % job_offer_id)
+    job_offer = get_object_or_404(JobOffer, pk=job_offer_id)
+    return render(request, 'WelcomeLiteApp/edit_offer.html', { 'job_offer': job_offer })
 
 def delete_offer(request, job_offer_id):
+    job_offer = get_object_or_404(JobOffer, pk=job_offer_id) 
     return HttpResponse("You're deleting offer %s" % job_offer_id)
