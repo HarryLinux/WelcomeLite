3704536796845083257d19269ab8aa97a7f19e57
Harold Fich <haroldfich@gmail.com>
2020-10-13 23:29:48 -0400

    Wire up index with db data and basic template for view

--- WelcomeLiteApp/views.py
+++ WelcomeLiteApp/views.py
@@ -1,8 +1,14 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import JobOffer
 
 def index(request):
-    return HttpResponse("Hello, world. From index WelcomeLite index view")
+    job_offer_list = JobOffer.objects.all()
+    template = loader.get_template('WelcomeLiteApp/index.html')
+    context = { 'job_offer_list': job_offer_list }
+    return HttpResponse(template.render(context, request))
 
 def add_offer(request):
     return HttpResponse("We made it to the add offers page!")
