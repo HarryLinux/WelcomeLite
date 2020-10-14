228d44d8402e05482507f1567d976b28881d374a
Harold Fich <haroldfich@gmail.com>
2020-10-14 01:35:34 -0400

    Delete offer basic functionality in views

--- WelcomeLiteApp/views.py
+++ WelcomeLiteApp/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.urls import reverse
 
 from .models import JobOffer
 
@@ -16,5 +17,16 @@ def edit_offer(request, job_offer_id):
     return render(request, 'WelcomeLiteApp/edit_offer.html', { 'job_offer': job_offer })
 
 def delete_offer(request, job_offer_id):
-    job_offer = get_object_or_404(JobOffer, pk=job_offer_id) 
-    return HttpResponse("You're deleting offer %s" % job_offer_id)
+    # if job offer does not exist, return to index with error message
+    # else, delete job offer and return to index
+    try:
+        job_offer = JobOffer.objects.get(pk=job_offer_id)
+    except (KeyError, JobOffer.DoesNotExist):
+        # Return to job offer list
+        return render(request, 'WelcomeLiteApp/index.html', {
+            'error_message': 'Job offer does not exist.'
+        })
+    else:
+        candiate_name = str(job_offer)
+        job_offer.delete()
+        return HttpResponseRedirect(reverse('welcome:index'))
