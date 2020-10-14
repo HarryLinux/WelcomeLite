c4e5c4fcf5d1172a8d34ba485af21ea06f125416
Harold Fich <haroldfich@gmail.com>
2020-10-13 21:59:45 -0400

    local postgres setup with testuser and JobOffer model

--- WelcomeLite/settings.py
+++ WelcomeLite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'WelcomeLiteApp.apps.WelcomeliteappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'WelcomeLite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'welcomedb',
+        'USER': 'testuser',
+        'PASSWORD': 'secret',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -105,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EST'
 
 USE_I18N = True
 
--- WelcomeLiteApp/models.py
+++ WelcomeLiteApp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class JobOffer(models.Model):
+    candidate_first_name = models.CharField(max_length=64)
+    candidate_last_name = models.CharField(max_length=64)
+    candidate_email = models.EmailField(max_length=254)
+    offer_amount = models.PositiveIntegerField(default=0)
