129fc345de81ff4e9c039194965525234f9435ff
Harold Fich <haroldfich@gmail.com>
2020-10-15 21:13:25 -0400

    Adding JobTitle model for relations

--- WelcomeLiteApp/admin.py
+++ WelcomeLiteApp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import JobOffer
+from .models import JobOffer, JobTitle
 
 admin.site.register(JobOffer)
+admin.site.register(JobTitle)
--- WelcomeLiteApp/models.py
+++ WelcomeLiteApp/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
+class JobTitle(models.Model):
+    job_title = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.job_title
+
 class JobOffer(models.Model):
     candidate_first_name = models.CharField(max_length=64)
     candidate_last_name = models.CharField(max_length=64)
-    candidate_email = models.EmailField(max_length=254)
-    job_title = models.CharField(max_length=254)
+    candidate_email = models.EmailField(max_length=256)
+    job_title = models.ForeignKey(JobTitle, on_delete=models.CASCADE)
     offer_amount = models.PositiveIntegerField(default=0)
 
     def __str__(self):
