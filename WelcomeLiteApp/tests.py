9c568c2a4a2dc0a9f8f2c5eb774a14fc46ac4eee
Harold Fich <haroldfich@gmail.com>
2020-10-19 23:18:52 -0400

    Simple index view test

--- WelcomeLiteApp/tests.py
+++ WelcomeLiteApp/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+class JobOfferIndexViewTests(TestCase):
+    def test_no_job_offers(self):
+        """
+        If no job offers exist, an appropriate message is displayed.
+        """
+        response = self.client.get(reverse('welcome:index'))
+        self.assertContains(response, "No job offers available")
